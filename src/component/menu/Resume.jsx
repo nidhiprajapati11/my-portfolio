33ab4d492c66faa880a7e827657f8bd1ad97dbc5
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2024-11-09 14:58:56 +0530

    change project data

--- src/component/menu/Resume.jsx
+++ src/component/menu/Resume.jsx
@@ -56,10 +56,10 @@ function Resume() {
                 <p>sparks to ideas.</p>
 
                 <ul>
-                  <li>work on react js this project is a event planner. like wedding, sports tec.</li>
-                  <li>when admin login user list should show whiche user registrated. user side show when admin post event.</li>
-                  <li>admin monitors all events.</li>
-                  <li>user is show the all event. the user click on a particular event and the details are display. user can book the show.</li>
+                  <li>This project is an event planner built with React.js, designed for events like weddings, sports, etc.</li>
+                  <li>When an admin logs in, a list of registered users should be displayed. On the user side, events posted by the admin will be visible.</li>
+                  <li>The admin can monitor all events, and users can view all available events. When a user clicks on a particular event, its details will be displayed, and the user can book the event.</li>
+                  {/* <li>user is show the all event. the user click on a particular event and the details are display. user can book the show.</li> */}
                 </ul>
               </div>
 
@@ -69,9 +69,9 @@ function Resume() {
                 <p>K.D Polytechnic College</p>
 
                 <ul>
-                  <li>online grocery shopping that allows online purchasing of fruits, vegetables etc.</li>
-                  <li>you can select your choice of  fruits and vegetables from the grocery store.</li>
-                  <li>a lot of pepole have come to prefer buying their groceries online today. </li>
+                  <li>Online grocery shopping allows for the online purchase of fruits, vegetables, and more</li>
+                  <li>You can select your preferred fruits and vegetables from the grocery store. Many people today prefer buying their groceries online.</li>
+                  {/* <li>a lot of pepole have come to prefer buying their groceries online today. </li> */}
                 </ul>
               </div>
             </div>
